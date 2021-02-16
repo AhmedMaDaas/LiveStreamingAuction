8052f0ce64b9ae416a8eeb53d870587357eb0c59
unknown <ahmedhamadah973@gmail.com>
2021-02-16 02:07:01 +0200

    make it better

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(require('cors')());
 const io = require("socket.io")(server, {
   cors: {
     origin: "https://hexapi.tech/",
-    methods: ["GET", "POST", "OPTIONS"],
+    methods: ["GET", "POST", "OPTIONs"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
   }
